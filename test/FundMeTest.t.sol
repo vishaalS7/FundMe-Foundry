ad65e7554bff137bcc2715803b680038b77e936e
vishaalS7 <ivishaal1411@gmail.com>
2024-10-09 15:49:14 +0530

    foundry-Test

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() public {
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+    }
+
+    function testMinimumUsd() public view {
+        console.log("Welcome 0Glitchx");
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgsender() public view {
+        console.log(fundMe.i_owner());
+        console.log(address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
+}
