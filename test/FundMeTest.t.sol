049ae226660e7fab365100e36c8db6f746099fa8
vishaalS7 <ivishaal1411@gmail.com>
2024-10-10 18:11:26 +0530

    FundMe Foundry

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,17 +1,16 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.18;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test, console} from "../lib/forge-std/src/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
-import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() public {
-        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        DeployFundMe deployFundMe = new DeployFundMe();
-        fundMe = deployFundMe.run();
+        fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        // DeployFundMe deployFundMe = new DeployFundMe();
+        // fundMe = deployFundMe.run();
     }
 
     function testMinimumUsd() public view {
