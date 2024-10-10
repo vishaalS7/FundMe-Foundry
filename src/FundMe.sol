7069251db1641c8b29387267f60b212261a4677f
vishaalS7 <ivishaal1411@gmail.com>
2024-10-10 18:18:26 +0530

    updated FundMe

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -4,9 +4,9 @@ pragma solidity 0.8.19;
 // 2. Imports
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-import {PriceConverter} from "./PriceConverter.sol";
-
+import "./Priceconverter.sol";
 // 3. Interfaces, Libraries, Contracts
+
 error FundMe__NotOwner();
 
 contract FundMe {
